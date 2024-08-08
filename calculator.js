ff2d3b6b4addc3334bf1a98d15b51fd20de7f063
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-08 18:15:25 +0200

    Add button functionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,26 @@
+let displayValue = "0";
+
+let storedNumber1 = 0;
+let storedNumber2 = 0;
+let storedOperation = "";
+
+const result = document.querySelector(".result-input");
+
+const oneBtn = document.querySelector("#oneBtn").addEventListener("click", function() {numberButton(1);});
+const twoBtn = document.querySelector("#twoBtn").addEventListener("click", function() {numberButton(2);});
+const threeBtn = document.querySelector("#threeBtn").addEventListener("click", function() {numberButton(3);});
+const fourBtn = document.querySelector("#fourBtn").addEventListener("click", function() {numberButton(4);});
+const fiveBtn = document.querySelector("#fiveBtn").addEventListener("click", function() {numberButton(5);});
+const sixBtn = document.querySelector("#sixBtn").addEventListener("click", function() {numberButton(6);});
+const sevenBtn = document.querySelector("#sevenBtn").addEventListener("click", function() {numberButton(7);});
+const eightBtn = document.querySelector("#eightBtn").addEventListener("click", function() {numberButton(8);});
+const nineBtn = document.querySelector("#nineBtn").addEventListener("click", function() {numberButton(9);});
+const zeroBtn = document.querySelector("#zeroBtn").addEventListener("click", function() {numberButton(0);});
+
+const clearBtn = document.querySelector("#clearBtn").addEventListener("click", clearDisplay);
+const resultBtn = document.querySelector("#resultBtn").addEventListener("click", clearDisplay);
+
+
 function add(number1, number2)
 {
     return number1 + number2
@@ -18,7 +41,7 @@ function divide(number1, number2)
     return number1 / number2
 }
 
-function operate(number1, number2, operator)
+function operate()
 {
     let result = 0;
     switch (operator) {
@@ -38,4 +61,18 @@ function operate(number1, number2, operator)
             break;
     }
     return result;
+}
+
+function numberButton(number){
+    if(displayValue == 0)
+    {
+        displayValue = "";
+    }
+    displayValue += number;
+    result.value = displayValue;
+}
+
+function clearDisplay(){
+    displayValue = "0";
+    result.value = displayValue;
 }
